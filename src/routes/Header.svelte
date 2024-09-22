eb13a88114d76f4f2091cb05847337876a7ec86a
alexw005 <fswoon2@gmail.com>
2024-09-22 22:43:58 +0800

    add title and desc

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -4,13 +4,13 @@
 
 <header>
 	<nav class="flex items-center p-2 space-x-4 lg:space-x-6">
-		<a href="/" class="hover:text-primary text-lg font-medium transition-colors"> Home </a>
+		<a href="/" class="hover:text-primary text-lg font-medium transition-colors"> Loan </a>
 
-		<a
+		<!-- <a
 			href="/about"
 			class="text-muted-foreground hover:text-primary text-lg font-medium transition-colors"
 		>
 			About
-		</a>
+		</a> -->
 	</nav>
 </header>
