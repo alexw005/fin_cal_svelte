41596e815d78a19851a771240522ad6cb44a7cf3
Alex <fswoon2@gmail.com>
2024-09-29 01:16:37 -0700

    fix height issue

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -3,14 +3,7 @@
 </script>
 
 <header>
-	<nav class="flex items-center p-4 space-x-4 lg:space-x-6 box">
+	<nav class="absolute p-4 space-x-4 lg:space-x-6 box">
 		<a href="/" class="hover:text-primary text-lg font-medium transition-colors"> Loan </a>
-
-		<!-- <a
-			href="/about"
-			class="text-muted-foreground hover:text-primary text-lg font-medium transition-colors"
-		>
-			About
-		</a> -->
 	</nav>
 </header>
