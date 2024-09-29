2af0fc8e3d589e3f8a7822264a7d2d81f4028c6c
alexw005 <fswoon2@gmail.com>
2024-09-30 00:49:48 +0800

    add sticky header bar

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <header>
-	<nav class="absolute p-4 space-x-4 lg:space-x-6 box">
+	<nav class="flex-no-wrap fixed top-0 z-10 flex w-full p-4 gap-4 items-center bg-[#FBFBFB] py-2 shadow-md shadow-black/5 dark:bg-neutral-600 dark:shadow-black/10 lg:flex-wrap lg:justify-start lg:py-4">
 		<a href="/" class="hover:text-primary text-lg font-medium transition-colors"> Loan </a>
 	</nav>
 </header>
